8d9d16a61630331b299e8c2433c5012294c06764
Brett Lesnau <lesnaubr@gmail.com>
2013-11-27 01:42:38 -0500

    formatting

--- Project/ProjectView.cpp
+++ Project/ProjectView.cpp
@@ -23,126 +23,132 @@
 
 IMPLEMENT_DYNCREATE(CProjectView, CView)
 
-BEGIN_MESSAGE_MAP(CProjectView, CView)
-   ON_COMMAND(ID_IMAGE_OPEN, &CProjectView::OnImageOpen)
-   ON_COMMAND(ID_IMAGE_RESET, &CProjectView::OnImageReset)
-   ON_COMMAND(ID_SELECTION_CLEAR, &CProjectView::OnSelectionClear)
-   ON_COMMAND(ID_PROCESS_PROCESS_ENTIRE_IMAGE, &CProjectView::OnProcessEntireImage)
-   ON_COMMAND(ID_PROCESS_PROCESS_SELECTION, &CProjectView::OnProcessSelection)
-END_MESSAGE_MAP()
+   BEGIN_MESSAGE_MAP(CProjectView, CView)
+      ON_COMMAND(ID_IMAGE_OPEN, &CProjectView::OnImageOpen)
+      ON_COMMAND(ID_IMAGE_RESET, &CProjectView::OnImageReset)
+      ON_COMMAND(ID_SELECTION_CLEAR, &CProjectView::OnSelectionClear)
+      ON_COMMAND(ID_PROCESS_PROCESS_ENTIRE_IMAGE, &CProjectView::OnProcessEntireImage)
+      ON_COMMAND(ID_PROCESS_PROCESS_SELECTION, &CProjectView::OnProcessSelection)
+      ON_COMMAND(ID_SELECTION_AUTOSELECT, &CProjectView::OnSelectionAutoselect)
+   END_MESSAGE_MAP()
 
-// CProjectView construction/destruction
+   // CProjectView construction/destruction
 
-CProjectView::CProjectView()
-{
-   m_bValidImage = FALSE;
-}
-
-CProjectView::~CProjectView()
-{
-}
-
-BOOL CProjectView::PreCreateWindow(CREATESTRUCT& cs)
-{
-	// TODO: Modify the Window class or styles here by modifying
-	//  the CREATESTRUCT cs
-
-	return CView::PreCreateWindow(cs);
-}
-
-void CProjectView::OnInitialUpdate()
-{
-   LoadNewImage( L"DefaultPic.jpeg" );
-}
-
-// CProjectView drawing
-void CProjectView::OnDraw(CDC* pDC)
-{
-	CProjectDoc* pDoc = GetDocument();
-	ASSERT_VALID(pDoc);
-	if (!pDoc)
-		return;
-
-   if( m_bValidImage )
+   CProjectView::CProjectView()
    {
-      CImage drawnImage;
-      drawnImage.Create( m_image.GetWidth(), m_image.GetHeight(), m_image.GetBPP() );
-      m_image.BitBlt( drawnImage.GetDC(), 0, 0 );
-
-      drawnImage.Draw( pDC->GetSafeHdc(), 0, 0 );
-
-      drawnImage.ReleaseDC();
-   }
-}
-
-void CProjectView::LoadNewImage( CString strFilePath )
-{
-   if( m_bValidImage )
-   {
-      m_image.Destroy();
+      m_bValidImage = FALSE;
    }
 
-   m_bValidImage = FALSE;
-
-   if( m_image.Load( strFilePath ) == S_OK )
+   CProjectView::~CProjectView()
    {
-      m_bValidImage = TRUE;
-   }
-   else
-   {
-      AfxMessageBox(L"Could not open image");
    }
 
-   this->RedrawWindow();
-}
-
-void CProjectView::OnImageOpen()
-{
-   CFileDialog fileDlg(true, NULL, NULL, OFN_FILEMUSTEXIST, L"Bitmap format|*.bmp|JPEG format|*.jpg;*.jpeg|GIF format|*.gif|PNG format|*.png|TIFF format|*.tif;*.tiff|");
-
-   if(fileDlg.DoModal() != IDOK)
+   BOOL CProjectView::PreCreateWindow(CREATESTRUCT& cs)
    {
-      return;
+      // TODO: Modify the Window class or styles here by modifying
+      //  the CREATESTRUCT cs
+
+      return CView::PreCreateWindow(cs);
    }
 
-   LoadNewImage( fileDlg.GetPathName() );
-}
+   void CProjectView::OnInitialUpdate()
+   {
+      LoadNewImage( L"DefaultPic.jpeg" );
+   }
 
-void CProjectView::OnImageReset()
-{
+   // CProjectView drawing
+   void CProjectView::OnDraw(CDC* pDC)
+   {
+      CProjectDoc* pDoc = GetDocument();
+      ASSERT_VALID(pDoc);
+      if (!pDoc)
+         return;
 
-}
+      if( m_bValidImage )
+      {
+         CImage drawnImage;
+         drawnImage.Create( m_image.GetWidth(), m_image.GetHeight(), m_image.GetBPP() );
+         m_image.BitBlt( drawnImage.GetDC(), 0, 0 );
 
-void CProjectView::OnSelectionClear()
-{
+         drawnImage.Draw( pDC->GetSafeHdc(), 0, 0 );
 
-}
+         drawnImage.ReleaseDC();
+      }
+   }
 
-void CProjectView::OnProcessEntireImage()
-{
+   void CProjectView::LoadNewImage( CString strFilePath )
+   {
+      if( m_bValidImage )
+      {
+         m_image.Destroy();
+      }
 
-}
+      m_bValidImage = FALSE;
 
-void CProjectView::OnProcessSelection()
-{
+      if( m_image.Load( strFilePath ) == S_OK )
+      {
+         m_bValidImage = TRUE;
+      }
+      else
+      {
+         AfxMessageBox(L"Could not open image");
+      }
 
-}
+      this->RedrawWindow();
+   }
 
-// CProjectView diagnostics
+   void CProjectView::OnImageOpen()
+   {
+      CFileDialog fileDlg(true, NULL, NULL, OFN_FILEMUSTEXIST, L"Bitmap format|*.bmp|JPEG format|*.jpg;*.jpeg|GIF format|*.gif|PNG format|*.png|TIFF format|*.tif;*.tiff|");
+
+      if(fileDlg.DoModal() != IDOK)
+      {
+         return;
+      }
+
+      LoadNewImage( fileDlg.GetPathName() );
+   }
+
+   void CProjectView::OnImageReset()
+   {
+
+   }
+
+   void CProjectView::OnSelectionAutoselect()
+   {
+
+   }
+
+   void CProjectView::OnSelectionClear()
+   {
+
+   }
+
+   void CProjectView::OnProcessEntireImage()
+   {
+
+   }
+
+   void CProjectView::OnProcessSelection()
+   {
+
+   }
+
+   // CProjectView diagnostics
 #ifdef _DEBUG
-void CProjectView::AssertValid() const
-{
-	CView::AssertValid();
-}
+   void CProjectView::AssertValid() const
+   {
+      CView::AssertValid();
+   }
 
-void CProjectView::Dump(CDumpContext& dc) const
-{
-	CView::Dump(dc);
-}
+   void CProjectView::Dump(CDumpContext& dc) const
+   {
+      CView::Dump(dc);
+   }
 
-CProjectDoc* CProjectView::GetDocument() const // non-debug version is inline
-{
-	ASSERT(m_pDocument->IsKindOf(RUNTIME_CLASS(CProjectDoc)));
-	return (CProjectDoc*)m_pDocument;
-}
+   CProjectDoc* CProjectView::GetDocument() const // non-debug version is inline
+   {
+      ASSERT(m_pDocument->IsKindOf(RUNTIME_CLASS(CProjectDoc)));
+      return (CProjectDoc*)m_pDocument;
+   }
 #endif //_DEBUG
